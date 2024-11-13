56b40ac7cd2cba07bf05ade86b115c221bcc1412
unknown <jeevanr2431@gmail.com>
2024-11-13 18:45:43 +0530

    add server functionality with user authentication and database connection

--- server/index.js
+++ server/index.js
@@ -1,21 +1,71 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import { connectDB } from './db.js';
+import User from './models/user.js';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import bcrypt from 'bcryptjs';
+
+dotenv.config();
 
 const app = express();
-const PORT = 3000;
 
+const PORT = process.env.PORT ;
+connectDB();
 // Middleware
 app.use(bodyParser.json());
-
+app.use(cors());
 // Routes
-app.post('/signup', (req, res) => {
+app.post('/signup', async (req, res) => {
     const { username, email, password } = req.body;
-    res.send('Signup route');
+    try {
+        const userExists = await  User.findOne({ email });
+        if (userExists) {
+            res.status(400);
+            throw new Error("User already exists");
+        }
+        const user = await User.create({
+            username,
+            email,
+            password
+        });
+        if (user) {
+            res.status(201).json({
+                _id: user._id,
+                username: user.username,
+                email: user.email,
+            });
+        } else {
+            res.status(400);
+            throw new Error("Invalid user data");
+        }
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message });
+    }
 });
 
-app.post('/signin', (req, res) => {
-    const { username, password } = req.body;
-    res.send('Signin route');
+app.post('/signin', async (req, res) => {
+    const {username,password} = req.body;
+    try{
+        const userExists = await  User.findOne({ username });
+        if (!userExists) {
+            return res.status(400).json({ message: "Invalid credentials!" });
+        }
+        const isMatch = await bcrypt.compare(password, userExists.password);
+        if (!isMatch) {
+            return res.status(400).json({ message: "Wrong password" });
+        }
+        res.json({
+            _id: userExists._id,
+            username: userExists.username,
+            email: userExists.email,
+        });
+    } catch (e) {
+        res.status(500).json({
+            message: "Unable to sign in"
+        });
+}
 });
 
 app.listen(PORT, () => {
